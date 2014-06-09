4fb07cfccaa1dcb7b40424fb21e39946bc5fa1a8
Mark Nickel <mark.nickel@gmail.com>
2014-06-09 16:12:45 -0500

    Completed the module:  Using Alloy Added a few advanced concepts for dismissing the keyboard that wasn't covered in the module.  There is TONS of variations on how people do this.

--- Resources/iphone/alloy/controllers/index.js
+++ Resources/iphone/alloy/controllers/index.js
@@ -2,6 +2,7 @@ function Controller() {
     function addAmount() {
         total += parseInt($.amountField.value);
         $.totalLabel.text = total;
+        $.amountField.blur();
     }
     function saveGoal() {
         Ti.API.info("goalField Text: " + $.goalField.value + "----");
@@ -12,6 +13,9 @@ function Controller() {
         null == goal && (goal = 0);
         $.goalLabel.text = goal;
         $.goalField.value = goal;
+    }
+    function doneGoal() {
+        $.goalField.blur();
     }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "index";
@@ -59,6 +63,7 @@ function Controller() {
         width: 100,
         keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD,
         borderStyle: Ti.UI.INPUT_BORDERSTYLE_BEZEL,
+        returnKeyType: Ti.UI.RETURNKEY_DONE,
         id: "amountField"
     });
     $.__views.__alloyId3.add($.__views.amountField);
@@ -165,10 +170,17 @@ function Controller() {
         width: 100,
         keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD,
         borderStyle: Ti.UI.INPUT_BORDERSTYLE_BEZEL,
+        returnKeyType: Ti.UI.RETURNKEY_DONE,
         id: "goalField"
     });
     $.__views.__alloyId9.add($.__views.goalField);
     saveGoal ? $.__views.goalField.addEventListener("change", saveGoal) : __defers["$.__views.goalField!change!saveGoal"] = true;
+    $.__views.goalDoneButton = Ti.UI.createButton({
+        title: "Done",
+        id: "goalDoneButton"
+    });
+    $.__views.__alloyId9.add($.__views.goalDoneButton);
+    doneGoal ? $.__views.goalDoneButton.addEventListener("click", doneGoal) : __defers["$.__views.goalDoneButton!click!doneGoal"] = true;
     $.__views.__alloyId8 = Ti.UI.createTab({
         window: $.__views.__alloyId9,
         title: "Settings",
@@ -189,6 +201,7 @@ function Controller() {
     __defers["$.__views.addButton!click!addAmount"] && $.__views.addButton.addEventListener("click", addAmount);
     __defers["$.__views.__alloyId1!focus!updateGoal"] && $.__views.__alloyId1.addEventListener("focus", updateGoal);
     __defers["$.__views.goalField!change!saveGoal"] && $.__views.goalField.addEventListener("change", saveGoal);
+    __defers["$.__views.goalDoneButton!click!doneGoal"] && $.__views.goalDoneButton.addEventListener("click", doneGoal);
     _.extend($, exports);
 }
 
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,34 @@
+var total = 0;
+
 $.index.open();
+updateGoal();
+
+function addAmount() {
+	total += parseInt($.amountField.value);
+	$.totalLabel.text = total;
+	$.amountField.blur();
+}
+
+function saveGoal() {
+	//Ti.API.info("goalField Text: " + $.goalField.value + "----");
+    
+    // only parse something if it makes sense!!
+	if($.goalField.value) {
+		Ti.App.Properties.setInt('goal', $.goalField.value);
+	}
+}
+
+function updateGoal() {
+	var goal = Ti.App.Properties.getInt('goal');
+	if( goal == null) {
+		goal = 0;
+	}
+	
+	$.goalLabel.text = goal;
+	$.goalField.value = goal;
+}
+
+function doneGoal() {
+	//Get rid of the keyboard by blurring the field.
+	$.goalField.blur();
+}
